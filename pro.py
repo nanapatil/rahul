d94d4957e96a2c36cc97009526cb7412a68cc437
nana <dhokanednyaneshwar55@gmail.com>
2022-12-29 11:31:46 -0800

    Commit message.

---
+++ pro.py
@@ -0,0 +1,4 @@
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+avg= (a+b)/2 
+print("the avarage of first and second number is",avg)
---
+++ string.py
@@ -0,0 +1,11 @@
+#string
+
+a=24
+b='harry'
+print(a,b)
+print(type(a))
+print(type(b))
+print(b[2:5]) #slicing
+
+print("#############################################################################")
+print(b[-1:-5])
